8bcc58b544661c1d6b59578eedf3bdaf399a64e8
pelemen20051 <122674982+pelemen20051@users.noreply.github.com>
2026-05-08 14:06:52 +0300

    добавил коментарии

--- ShoesDE/Statics/CurrentSession.cs
+++ ShoesDE/Statics/CurrentSession.cs
@@ -7,8 +7,13 @@ using System.Threading.Tasks;
 
 namespace ShoesDE.Statics
 {
-     public static class CurrentSession
+     public static class CurrentSession // Логически группирует классы, относящиеся к статическим данным (например, глобальные состояния). 
+     //Предназначен для хранения глобальных данных, доступных из любого места приложения.
     {
-        public static User CurrentUser { get; set; }
+        public static User CurrentUser { get; set; }//Статическое автоматическое свойство. Хранит объект типа User (предположительно, сущность пользователя из базы данных).
+
+//get; set; — можно прочитать (User user = CurrentSession.CurrentUser;) и записать (CurrentSession.CurrentUser = someUser;).
+
+//Поскольку класс статический, свойство существует в единственном экземпляре на всё приложение.
     }
 }
